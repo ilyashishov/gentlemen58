f8f710045f6c401b6650285b62728b9b2bf91b8d
IlyaShishov <stalk1258@yandex.ru>
2016-09-29 01:36:10 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp = require("gulp");
+var less = require('gulp-less');
+var path = require('path');
+var imagemin = require('gulp-imagemin');
+const babel = require('gulp-babel');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+
+
+
+gulp.task('minifyJS', ['js'], function (cb) {
+  pump([
+        gulp.src('./js/**/*.js'),
+        uglify(),
+        gulp.dest('./js/')
+    ],
+    cb
+  );
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('./less/**/*.less', ['minify-css']);
+    gulp.watch('./js_source/**/*.js', ['minifyJS']);
+});
+
+gulp.task('images', function() {
+    return gulp.src('./img_source/**/*')
+            .pipe(imagemin({optimizationLevel: 5}))
+            .pipe(gulp.dest('./img'));
+});
+
+gulp.task('less', function () {
+  return gulp.src('./less/**/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('minify-css', ['less'],  function() {
+  return gulp.src('./css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('js', () => {
+  return gulp.src('./js_source/**/*')
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('default', ['watch', 'images', 'minify-css', 'minifyJS']);
